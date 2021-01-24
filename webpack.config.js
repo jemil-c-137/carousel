3da732be5dc3d552d2596c0626921462974b8bf6
Jemil <jemil1931@gmail.com>
2021-01-24 12:52:32 +0300

    initialize application

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/App.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+};
